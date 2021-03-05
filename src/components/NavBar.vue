36c7d1f5969cb95b3c81b9119d599b455674c0f5
Carmelo <melonemark8@gmail.com>
2021-03-05 17:47:00 +0100

    add logo and title

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,16 @@
 <template>
-<div id="navbar"></div>  
+<div id="navbar">
+  <div id="logo">
+    <div id="logo-image">
+      <router-link to="Home">
+        <img src="..\assets\logo.png">
+      </router-link>
+    </div>
+    <div id="logo-title">
+      <h1>Campagna Vaccini</h1>
+    </div>
+  </div>
+</div>  
 </template>
 
 <script>
@@ -15,5 +26,40 @@ name: "Navbar"
   background-color: #49bd96;
   position: fixed;
   top: 0;
+}
+
+#logo {
+  position: fixed;
+  display: inline-block;
+}
+
+#logo-image {
+  position: fixed;
+  left: 0;
+  border-radius: 50%;
+  padding: 0.3%;
+}
+
+
+img {
+  width: 70px;
+}
+
+#logo-title {
+  color: white;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  text-align: initial;
+  position: fixed;
+  left: 5vw;
+  padding-left: 1%;
+}
+
+@media only screen and (max-width: 768px) {
+  
+  #logo-title {
+    left: 18vw;
+    font-size: medium;
+  }
+
 }
 </style>
